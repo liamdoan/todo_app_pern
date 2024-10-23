e978a902357e1201de5e4110e10497d2e477835b
Lam Doan <lam.doan@dreambroker.fi>
2024-10-23 11:58:54 +0300

    basic setup for node and mongo

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors')
+const dotenv = require('dotenv'); //keep keys confidential
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+dotenv.config();
+
+const routes = require('./routes/ToDoRoute')
+
+mongoose
+.connect(process.env.MONGO_URL)
+.then (() => {
+    console.log('Connected to MongoDB')
+})
+.catch((err) => {
+    console.log(err)
+});
+
+app.use(routes)
+
+app.listen(PORT, () => {
+    console.log(`server is up, listening on port ${PORT}`);
+});
