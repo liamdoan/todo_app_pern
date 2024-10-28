ee74a84a560f58b4a1d462a66966c7704219e07a
liamdoan <lamdoan9898@gmail.com>
2024-10-28 16:58:52 +0200

    differentiate test and production api

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,1 +1,5 @@
-export const baseUrl = 'http://localhost:5000/api';
+// for local test and development
+// export const baseUrl = 'http://localhost:5000/api';
+
+// for after deploying to server, use this url instead
+export const baseUrl = 'https://todo-app-mern-backendld.vercel.app/api';
