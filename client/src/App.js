ce6777e54b90af4267892c4db3d8f1578924277b
liamdoan <lamdoan9898@gmail.com>
2024-10-31 01:13:08 +0200

    fix prettier

--- client/src/App.js
+++ client/src/App.js
@@ -134,13 +134,13 @@ const App = () => {
                         {todoEditing === todo._id ? (
                             <div className="input-edit-wrap">
                                 <input
-                                    className='input-edit-task'
+                                    className="input-edit-task"
                                     type="text"
                                     onChange={(e) => setEditingText(e.target.value)}
                                     value={editingText}
                                 />
                                 <input
-                                    className='input-edit-desc'
+                                    className="input-edit-desc"
                                     type="text"
                                     onChange={(e) => setEditingDesc(e.target.value)}
                                     value={editingDesc}
